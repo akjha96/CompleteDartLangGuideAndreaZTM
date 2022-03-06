b3e1cd33cd94b30244fe2c0b13223aec4420a789
Anand Jha <akjha96@gmail.com>
2022-03-06 10:18:18 +0530

    Future using async await syntax

--- Section16AsyncProgramming/futures.dart
+++ Section16AsyncProgramming/futures.dart
@@ -3,10 +3,25 @@ Future<String> fetchUserOrder() => Future.delayed(
       () => 'Cappuccino',
     );
 
-void main() {
-  print("Program Started");
-  fetchUserOrder()
-      .then((order) => print('Order is ready: $order'))
-      .catchError((error) => print(error))
-      .whenComplete(() => print('Done'));
+// void main() {
+//   print("Program Started");
+//   fetchUserOrder()
+//       .then((order) => print('Order is ready: $order'))
+//       .catchError((error) => print(error))
+//       .whenComplete(() => print('Done'));
+// }
+
+//Future using async await
+Future<void> main() async {
+  print('Program Started');
+  try {
+    final order = await fetchUserOrder();
+    print(order);
+    final order2 = await fetchUserOrder();
+    print(order2);
+  } catch (e) {
+    print(e);
+  } finally {
+    print('Done');
+  }
 }
