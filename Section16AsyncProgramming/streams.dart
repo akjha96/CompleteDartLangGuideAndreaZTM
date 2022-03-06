f4eb501e6d06e12c88e4ad7ca9a54714558380b6
Anand Jha <akjha96@gmail.com>
2022-03-06 20:36:27 +0530

    Stream generator async, yield & sync, Iterable

--- Section16AsyncProgramming/streams.dart
+++ Section16AsyncProgramming/streams.dart
@@ -9,8 +9,26 @@ Future<int> sumStream(Stream<int> stream) async {
 Future<int> sumStream2(Stream<int> stream) =>
     stream.reduce((previous, element) => previous + element);
 
+//async generator
+Stream<int> countStream(int n) async* {
+  // some async work
+  for (var i = 1; i <= n; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    print(i);
+    yield i;
+  }
+}
+
+// sync generator
+Iterable<int> count(int n) sync* {
+  for (var i = 1; i <= n; i++) {
+    yield i;
+  }
+}
+
 Future<void> main() async {
   final stream = Stream<int>.fromIterable([1, 2, 3, 4]);
-  final sum = await sumStream2(stream);
+  final stream2 = countStream(4);
+  final sum = await sumStream2(stream2);
   print('Sum: $sum');
 }
