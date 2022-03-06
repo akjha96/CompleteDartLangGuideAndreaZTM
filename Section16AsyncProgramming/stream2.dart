c714d38720be79167d504d06d1a2a0b2c6d41602
Anand Jha <akjha96@gmail.com>
2022-03-06 21:04:54 +0530

    more Stream methods for Iterable

--- Section16AsyncProgramming/stream2.dart
+++ Section16AsyncProgramming/stream2.dart
@@ -1,7 +1,16 @@
-void main() {
-  Stream.fromIterable([1, 2, 3, 4]);
-  Stream.value(10);
-  Stream.error(UnsupportedError('Error not Handled'));
-  Stream.fromFuture((Future.delayed(Duration(seconds: 1), () => 42)));
-  Stream.periodic(Duration(seconds: 1), (index) => index);
+Future<void> main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4]);
+  // Stream.value(10);
+  // Stream.error(UnsupportedError('Error not Handled'));
+  // Stream.fromFuture((Future.delayed(Duration(seconds: 1), () => 42)));
+  // Stream.periodic(Duration(seconds: 1), (index) => index);
+
+  // final value = await (stream.first);
+  // print(value);
+
+  // await stream.forEach((element) => print(element));
+
+  final doubles = stream.map((value) => value * 2).where((value) => value > 3);
+  // print(doubles);
+  await doubles.forEach(print);
 }
